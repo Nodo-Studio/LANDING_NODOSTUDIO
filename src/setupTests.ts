9a28823437c6566f7e07a3009baa02e5ff00f8ef
JDAAR <jdanaconar@gmail.com>
2025-09-04 18:28:53 -0500

    fix(test) : mock de window.matchMedia

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom'
+import { vi } from 'vitest'
+
+//Mock window.matchmedia 
+Object.defineProperty(window, 'matchmedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
